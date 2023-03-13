b5cf3f982f20ef20d2a5d0032a50578d0674413e
Angeline Polidano <angelinepolidano@Angelines-MacBook-Pro.local>
2023-03-12 17:17:43 -0700

    Add EventSchema

---
+++ server/Model/EventSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const EventSchema = new mongoose.Schema({
+ eventName : {
+        type : String,
+        required : true
+    },
+  where: {
+    type : String,
+    required : true
+  },
+  description: {
+    type : String,
+    required : true
+  },
+  date: {
+    type : Date,
+    required : true
+  },
+})
+
+const Event = mongoose.model('Event', EventSchema)
+
+module.exports = Event
